ea32e82f98e37fa287439115fd714272fe26674a
vuhuyanhnew <vuhuyanhnew@gmail.com>
2024-10-08 15:43:35 +0700

    push CheckLeapYear

---
+++ CheckLeapYear/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int year;
+        bool isLeapYear = false;
+        Console.Write("Enter a year: ");
+        year = Convert.ToInt32(Console.ReadLine());
+           bool isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4)
+        {
+            bool isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100)
+            {
+                bool isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400)
+                {
+                    isLeapYear = true;
+                }
+            }
+            else
+            {
+                isLeapYear = true;
+            }
+            
+        }
+        
+        if (isLeapYear)
+        {
+            Console.WriteLine("{0} is a leap year", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is NOT a leap year", year);
+        }
+
+            
+    }
+}
